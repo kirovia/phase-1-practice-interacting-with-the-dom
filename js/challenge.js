1e54e73775d834117690390644e52d17d608b89b
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-11-19 00:09:27 -0500

    sets up index.html to start building out deliverables and adds basic variables for all DOM elements

---
+++ js/challenge.js
@@ -0,0 +1,12 @@
+
+// grabbing some elements from the DOM
+
+const counter = document.getElementById('counter');
+const minus = document.getElementById('minus');
+const plus = document.getElementById('plus');
+const heart = document.getElementById('heart');
+const pause = document.getElementById('pause');
+const form = document.getElementById('comment-form');
+const input = document.getElementById('comment-input');
+const submit = document.getElementById('submit');
+

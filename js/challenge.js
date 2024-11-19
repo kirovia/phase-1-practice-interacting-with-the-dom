d49eb6f720e524df02a5dbced67ec6a7182f2cdf
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-11-19 16:05:18 -0500

    adds functionality to heart button

--- js/challenge.js
+++ js/challenge.js
@@ -7,9 +7,12 @@ const minus = document.getElementById('minus');
 const plus = document.getElementById('plus');
 const heart = document.getElementById('heart');
 const pause = document.getElementById('pause');
+const likes = document.querySelector('.likes');
+const comments = document.getElementById('list');
 const form = document.getElementById('comment-form');
 const input = document.getElementById('comment-input');
 const submit = document.getElementById('submit');
+let likesCounterObject = {};
 
 // counter functionality
 
@@ -18,7 +21,22 @@ setInterval(() => {
     counterDisplay.innerText = counter;
 }, 1000);
 
-// increment & decrement counter manually
+// manually increment & decrement counter
 
 minus.addEventListener('click', () => {counter -= 1; counterDisplay.innerText = counter});
 plus.addEventListener('click', () => {counter += 1; counterDisplay.innerText = counter});
+
+// heart button
+
+heart.addEventListener('click', () => {
+    let allLikes = likes.children;
+    likesCounterObject[`${counter}`] ? likesCounterObject[`${counter}`] += 1 : likesCounterObject[`${counter}`] = 1;
+    const li = document.createElement('li');
+    li.setAttribute('name', `${counter}`);
+    li.innerText = `${counter} has been liked ${likesCounterObject[`${counter}`]} times`;
+    if (!!allLikes.namedItem(`${counter}`)) {
+        allLikes.namedItem(`${counter}`).innerText = `${counter} has been liked ${likesCounterObject[`${counter}`]} times`;
+    } else {
+        likes.append(li);
+    }
+});
